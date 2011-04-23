be73783f260985bb076fc7bc6624f3b403457fef
Second Planet <contact@secondplanetanimation.com>
2011-04-23 18:43:51 -0400

    cleaning up, almost functional installer to be bundled into an OSX .mpkg and build

--- installers/homebrewinstall.sh
+++ installers/homebrewinstall.sh
@@ -4,12 +4,12 @@ let hasbrew=0
 let hasgit= 0
 hasbrew=$(which brew)
 hasgit= $(which git)
-if [ $hasbrew = "" ]
+if [ $hasbrew = "/usr/local/bin/brew" ]
 then
 	sudo chown -R $USER /usr/local
 	curl -Lsf http://github.com/mxcl/homebrew/tarball/master | tar xz --strip 1 -C/usr/local
 else
-	brew update	
+	/usr/local/bin/brew update	
 fi
 if [ $hasgit = "" ]
 	brew install git
---
+++ installers/synfiginstall.sh
@@ -0,0 +1,3 @@
+brew install https://github.com/secondplanet/synfig-osx/raw/master/homebrew/synfigstudio.rb
+brew install ffmpeg
+brew install libdv
